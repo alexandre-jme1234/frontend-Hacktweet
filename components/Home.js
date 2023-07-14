547c93c9201ddc788193a204bd26fee6068e3c3d
jammealexandre28 <jammealexandre1@gmail.com>
2023-07-14 17:19:14 +0200

    front-end intégration

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,23 @@
 import styles from '../styles/Home.module.css';
+import { Button } from "antd";
 
 function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.backgroundImage} />
+        <div className={styles.loginGrid}>
+          <div className={styles.contentSide}>
+          <div className={styles.logo} alt="Logo" />
+          <h1 className={styles.title}>See What's happening</h1>
+          <h1 className={styles.subTitle}>Join Hackatweet today.</h1> 
+          </div>
+          <div className={styles.signBlock}>
+          <Button type="primary">Primary Button</Button>
+          <h1 className={styles.textTitle}>Already have an account ?</h1>
+          <Button type="default" >Secondary Button</Button> 
+          </div>
+        </div>
       </main>
     </div>
   );
